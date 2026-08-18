74106b682e6ac6c891356d318053eab4b16a55cf
Tushar Shihab <tushar.shihab13@gmail.com>
2026-08-18 23:56:01 +0600

    feat: secure private credentials

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
